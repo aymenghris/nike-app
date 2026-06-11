e9af65b0eca36b0a0b875b99bc4db6b1d11a98f7
aymenghris <aymen.quantum@gmail.com>
2026-06-11 19:08:59 +0100

    feat(assets): add light/dark rose backgrounds
    - Added two new public SVG background assets in `public/backgrounds/`
    - Updated `constants/assets-paths.ts` to extend ASSETS_PATHS with new background path constants

--- constants/assets-paths.ts
+++ constants/assets-paths.ts
@@ -2,4 +2,8 @@ export const ASSETS_PATHS = {
     navbar: {
         logo: "/navbar/logo.svg",
     },
+    backgrounds: {
+        light: "/backgrounds/light-rose.svg",
+        dark: "/backgrounds/dark-rose.svg",
+    },
 }
